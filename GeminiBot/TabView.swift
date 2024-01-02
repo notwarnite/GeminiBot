539c998ca97abed6e53779649044515a5466d913
Ritwik Singh <warnite@Ritwiks-MacBook-Pro.local>
2024-01-02 16:06:03 +0530

    update: tabView

--- GeminiBot/TabView.swift
+++ GeminiBot/TabView.swift
@@ -7,20 +7,34 @@
 
 import SwiftUI
 
+struct TabItem: Identifiable {
+    var id = UUID()
+    var dest: AnyView
+    var title: Text
+    var image: Image
+    var tag: Int
+}
+
 struct TabNav: View {
+    let tabData = [
+        TabItem(dest: AnyView(PromptWindow()), title: Text("PROMPT"), image: Image(systemName: "option"), tag: 1),
+        TabItem(dest: AnyView(ChatWindow()), title: Text("CHAT"), image: Image(systemName: "option"), tag: 2)
+    ]
+
+    @State private var selectedTab = 0
+
     var body: some View {
-        TabView {
-            PromptWindow()
-                .tabItem {
-                    Label("Prompt", systemImage: "list.dash")
-                }
-                .tag(1)
-            
-            ChatWindow()
-                .tabItem {
-                    Label("Chat", systemImage: "list.dash")
-                }
-                .tag(2)
+        TabView(selection: $selectedTab) {
+            ForEach(tabData) { tabItem in
+                tabItem.dest
+                    .tabItem {
+                        Label(
+                            title: { tabItem.title },
+                            icon: { tabItem.image }
+                        )
+                    }
+                    .tag(tabItem.tag)
+            }
         }
     }
 }
